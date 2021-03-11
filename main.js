2f63469398034aa22f6aec6c7eef0f3686d15491
nupur1509 <74081807+nupur1509@users.noreply.github.com>
2021-03-11 06:38:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function changecolor() {
+    document.getElementById("heading1").style.color = "yellow";
+}
+
+function changesize() {
+    document.getElementById("heading2").style.fontSize = "30px";
+}
